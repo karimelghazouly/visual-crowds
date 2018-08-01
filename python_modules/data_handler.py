ef294dfa3f2838c7f5cbe9587c8f80aad2f25c10
Ahmad <ahmad.hussain.307@gmail.com>
2018-08-01 21:46:59 +0200

    Added reading user csv

--- python_modules/data_handler.py
+++ python_modules/data_handler.py
@@ -1,12 +1,30 @@
 import googlemaps as gmaps
+import pandas as pd
 
-def get_adress(lat, lng):
+def get_adress_and_city(lat, lng):
     """Returns street address of the given location.
     :param lng: longitude of the user's location.
     :param lat: latitude of the user's location.
+    :return: pair of strings (street_address, city_name).
     """
     api = gmaps.Client(key='AIzaSyBRTtk15JKrytkz540PDf7ibEwIzhdKPa4')
 
     address = api.reverse_geocode({'lat':lat, 'lng':lng})
-    return address[0]['formatted_address']
+
+    # Return street address and city name
+    return address[0]['formatted_address'], address[0]['address_components'][2]['short_name']
+
+def read_fill_person_geo():
+    """Reads csv file of user's geo information and adds city and street."""
+    csv = pd.read_csv('D:/Projects/test.csv')
+
+    # Add street and city based on latitude and longitude
+    for i in range(0,len(csv)):
+        lat = csv['lat'][i]
+        lng = csv['lng'][i]
+        street, city = get_adress_and_city(lat, lng)
+
+        csv.loc[i,'city'] = city
+        csv.loc[i, 'street'] = street
+
 
