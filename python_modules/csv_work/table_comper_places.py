134a0bbc6218033a6365d36bc7b84ef29f0f71b4
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-08-03 00:18:52 +0200

    changed dir. and removed logs

--- python_modules/csv_work/table_comper_places.py
+++ python_modules/csv_work/table_comper_places.py
@@ -1,53 +1,54 @@
 import pandas as pd
 import csv
-s = pd.read_csv('geo_data.csv')
 
 
-def compare_place_to_people_in(data):
-    Geo = data
-    city = Geo.iloc[:,3]
+
+def compare_place_to_people_in():
+    print("first")
+    s = pd.read_csv('python_modules/csv_work/geo_data.csv')
+    Geo = s
+    city = Geo.iloc[:,3].values
     print(city)
-
-    comper = pd.read_csv('table.csv')
+    comper = pd.read_csv('python_modules/csv_work/table.csv')
     place = comper.iloc[:,0].values
     capicty_max = comper.iloc[:,1].values
-
+    #print(place)
     x = []
     for i in range(4):
         k = 0
         for j in range(len(city)):
+            print(place[i],city[j])
             if place[i] == city[j]:
                 k+=1
         x.append(k)
+        
+    print(x)
 #list append
 
     #Comperting
     complete = []
-    print(place)
-    print(x)
-    print(capicty_max)
+    #print(place)
+    #print(x)
+    #print(capicty_max)
     for f in range(len(x)):
         if x[f]>capicty_max[f]:
             if f==0:
-                print("overflow in Makka")
-                print("the current number im makka is"+str(x[f]))
+                #print("overflow in Makka")
+                #print("the current number im makka is"+str(x[f]))
                 complete.append(("Makka",capicty_max[f],x[f]))
             if f==1:
-                print("overflow in Arafa")
-                print("the current number im Arafa is" +str(x[f]))
+                #print("overflow in Arafa")
+                #print("the current number im Arafa is" +str(x[f]))
                 complete.append(("Arafa", capicty_max[f], x[f]))
             if f==2:
-                print("overflow in mena")
-                print("the current number im mena is" + str(x[f]))
+                #print("overflow in mena")
+                #print("the current number im mena is" + str(x[f]))
                 complete.append(("mena", capicty_max[f], x[f]))
 
             if f==3:
-                print("overflow in muzdalifah")
-                print("the current number im muzdalifah is" + str(x[f]))
+                #print("overflow in muzdalifah")
+                #print("the current number im muzdalifah is" + str(x[f]))
                 complete.append(("muzdalifah", capicty_max[f], x[f]))
 
-    print(complete)
+    #print(complete)
     return complete
-
-
-x=compare_place_to_people_in(s)
