0402d4f662cc8194773dd57c0f753116c1b19819
mostafa <mostafamousa1080p@gmail.com>
2018-08-02 17:46:28 +0200

    Adding compar funtion

--- python_modules/csv_work/generate_ID_lang_lat_city_st.py
+++ python_modules/csv_work/generate_ID_lang_lat_city_st.py
@@ -3,8 +3,10 @@ import csv
 from Generator import lng_lat
 #makka 3arf mozdlifa mena
 land = [[21.422487,39.826206], [21.3549, 39.9841], [21.231979,39.56959],[21.4146,39.8946]]
+places = ['makka','arafa','mena','muzdalifah']
 fake = Faker('ar_SA')
 x=0
+p=0
 for i in range(4):
     latitude,longitude = lng_lat(land[i][0],land[i][1])
     print(len(latitude))
@@ -14,7 +16,7 @@ for i in range(4):
         #writer.writeheader()
 
         for i in range(0,10):
-            writer.writerow({'id': str(x), 'lat': str(latitude[i]), 'long': str(longitude[i])})
+            writer.writerow({'id': str(x), 'lat': str(latitude[i]), 'long': str(longitude[i]) , 'city':str(places[p])})
             x+=1
-
+        p+=1
 print("Writing complete")
