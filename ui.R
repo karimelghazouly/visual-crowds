a4bd278ee65642db9fe7d6013dd1ee8e5b99a1f0
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-08-03 01:47:55 +0200

    Added facilites tab

--- ui.R
+++ ui.R
@@ -6,29 +6,33 @@ library(shinyjs)
 library(gapminder)
 library(leaflet)
 library(profvis)
+library(shinyjs)
 source_python('python_modules/data_handler.py')
 source_python('python_modules/csv_work/table_comper_places.py')
 ui <- fluidPage(
+  useShinyjs(),
   theme='theme.css',
   includeCSS('www/styles.css'),
   h1('Visual Crowds',style='margin-left:45%',class="text-success"),
-  actionButton('insert','insert'),
   tabsetPanel(id='tabs',
-              tabPanel(h4('Street View'),
+              tabPanel(id='strt',title=h4('Street View'),
                        actionButton('OverAll',h3('OverAll'),class="btn btn-primary"),
                        actionButton('Makka',h3('Makka'),class="btn btn-primary"),
                        actionButton('Arafa',h3('Arafa'),class="btn btn-primary"),
                        actionButton('Mena',h3('Mena'),class="btn btn-primary"),
                        leafletOutput('street')
               ),
-              tabPanel(h4('Search For Pilgrim'),
+              tabPanel(id='srching',h4('Search For Pilgrim'),
                        fixedRow(
                          column(2,radioButtons('search_choice',h4("Search by : "),choices = c("ID","Name"))),
                          column(4,textInput('input-srch',h4('Enter Value :')))),
                        leafletOutput('piligrim')
-              )
+              ),
+              tabPanel(id='fac',h4("Facilities"),
+                       leafletOutput('faclt')
+                       )
   ),
-  div( class='jumbotron',style="margin-top:5%;",
+  div( id='anal',class='jumbotron',style="margin-top:5%;",
        fixedRow(
          column(4,
                 p('Warnings',class='text-danger'),
